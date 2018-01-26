f32e378f779c86d4ec2d254f5569be40799825c2
Rocktim <rocktim53@gmail.com>
2018-01-26 23:37:37 +0530

    I can done

--- script/script.js
+++ script/script.js
@@ -2,6 +2,12 @@
 window.onscroll = function() {myFunction()};
 
 function myFunction() {
+	// alert(document.documentElement.scrollTop);
+	// if(document.documentElement.scrollTop>=392)
+	// 	document.getElementById("aboutdetails").style.marginTop="82px";
+	// else
+	// 	document.getElementById("aboutdetails").style.marginTop="32px";
+
     if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) {
         document.getElementById("fixheader").style.top = "0px";
     } else {
