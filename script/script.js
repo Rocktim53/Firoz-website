29e8fb3e6bc9bdd9a57b0529996dfb47e54b542d
Rocktim <rocktim53@gmail.com>
2018-02-01 02:22:01 +0530

    slideshow css and javascript done

--- script/script.js
+++ script/script.js
@@ -14,3 +14,46 @@ function myFunction() {
         document.getElementById("fixheader").style.top = "-45px";
     }
 }
+
+
+var slideIndex=1;
+var click=0;
+var time1;
+
+function plusSlide(n)
+{
+	click=1;
+	showSlides(slideIndex+=n);
+}
+
+function currentSlide(n)
+{
+	click=1;
+	showSlides(slideIndex=n);
+}
+
+function showSlides(n)
+{
+	var i;
+	var slides=document.getElementsByClassName("slideimg");
+	var dots=document.getElementsByClassName("dots");
+	if(n>slides.length)
+		slideIndex=1;
+	if(n<1)
+		slideIndex=slides.length;
+
+	for(i=0;i<slides.length;i++)
+	{
+	  slides[i].style.display="none";
+	  dots[i].className = dots[i].className.replace(" active"," ");
+	}
+
+	slides[slideIndex-1].style.display = "block";  
+  	dots[slideIndex-1].className += " active";
+
+  	if(click==0)
+  		time1=setTimeout(function(){showSlides(slideIndex+=1);},4000);
+  	else if(click==1)
+    	 click=0;
+
+}
