bca9d6077ca906be75d8ba204fc3381ea5cc7582
Rocktim <rocktim53@gmail.com>
2018-02-21 23:50:50 +0530

    php code done

--- submit.php
+++ submit.php
@@ -6,12 +6,17 @@
     $message = $_POST['message'];
     $email = $_POST['emailID'];
 
-    $mailmessage = 'New Message from '.$first.' '.$last.'<br/>'.$message.'<br>contact no '.$phone.'<br>Email '.$email;
+    $mailmessage = "You got a Message from ".$first." " .$last."\r\n\n".$message."\r\n\nContact no ".$phone."\r\nEmail ".$email;
+
+    $mailmessage2="Hello ".$first." ".$last.",\r\n\nGreetings from FIROZ'S MODULAR KITCHEN!\n\nThank you for your interest in Firoz's Modular, one of the latest Modular Kitchen outlets in Jorhat.\r\n\nFiroz's Modular Kitchen specializes in state of the art kitchen equipments and gadgets. Firoz Modular also helps in designing Modular Kitchens keeping in mind your visions for your cookhouse.\r\n\nWe'll contact you within 48hours with more details.";
     
     $to = 'contact@firozmodular.com';
     $subject = 'firozmodular';
     $headers = 'From: '.$email;
+    $headers2='From: '.$to;
         
     mail($to,$subject,$mailmessage,$headers);
+    mail($email,$subject,$mailmessage2,$headers2);
+
     header('Location: index.html');
 ?> 
