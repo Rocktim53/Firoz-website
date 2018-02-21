44f3d07a771590b31bebe71f292a79d0a0f31d6b
Rocktim <rocktim53@gmail.com>
2018-02-21 21:26:59 +0530

    minor change

--- script/script.js
+++ script/script.js
@@ -13,6 +13,73 @@ function myFunction() {
     } else {
         document.getElementById("fixheader").style.top = "-45px";
     }
+
+    // var height=document.documentElement.scrollHeight;
+   var winScroll =document.body.scrollTop || document.documentElement.scrollTop;
+     //var scrolled = (winScroll / height)*100;
+    //  console.log("Total height: "+document.documentElement.scrollHeight);
+    //  console.log("Client height: "+document.documentElement.clientHeight);
+    // console.log("difference height: "+height);
+    // console.log("scroll: "+winScroll);
+    // console.log("calculated: "+scrolled);
+   // document.getElementById("bar").style.width = scrolled + "%";
+
+    var home=document.getElementById("home");
+    var about=document.getElementById("aboutdetails");
+    var ican=document.getElementById("whatIcan");
+    var gallery=document.getElementById("gallery");
+    var contact=document.getElementById("contact");
+    var direction=document.getElementById("direction");
+
+    var fixheader=document.getElementsByClassName("fixheader");
+ 
+
+     if(winScroll<home.scrollHeight)
+     {
+     	bottomborderdisable();
+        fixheader[0].style.borderBottom="4px solid #4F82C3";
+     }
+
+     else if(winScroll>home.scrollHeight && winScroll<(about.scrollHeight+home.scrollHeight))
+     {
+       bottomborderdisable();
+       fixheader[1].style.borderBottom="4px solid #4F82C3";
+     }
+     else if(winScroll>(about.scrollHeight+home.scrollHeight) && winScroll<(about.scrollHeight+home.scrollHeight+ican.scrollHeight))
+     {
+     	bottomborderdisable();
+       fixheader[2].style.borderBottom="4px solid #4F82C3";
+     }
+     else if(winScroll>(about.scrollHeight+home.scrollHeight+ican.scrollHeight) && winScroll<(about.scrollHeight+home.scrollHeight+ican.scrollHeight+gallery.scrollHeight))
+     {
+     	bottomborderdisable();
+       fixheader[3].style.borderBottom="4px solid #4F82C3";
+     }
+     else if(winScroll>(about.scrollHeight+home.scrollHeight+ican.scrollHeight+gallery.scrollHeight) && winScroll<(about.scrollHeight+home.scrollHeight+ican.scrollHeight+gallery.scrollHeight+contact.scrollHeight+direction.scrollHeight))
+     {
+     	bottomborderdisable();
+       fixheader[4].style.borderBottom="4px solid #4F82C3";
+     }
+     else if (document.documentElement.scrollHeight - document.documentElement.scrollTop === document.documentElement.clientHeight)
+     {
+     	bottomborderdisable();
+       fixheader[6].style.borderBottom="4px solid #4F82C3";
+     }     
+     else if(winScroll>(about.scrollHeight+home.scrollHeight+ican.scrollHeight+gallery.scrollHeight+contact.scrollHeight) && winScroll<(about.scrollHeight+home.scrollHeight+ican.scrollHeight+gallery.scrollHeight+contact.scrollHeight))
+     {
+     	bottomborderdisable();
+       fixheader[5].style.borderBottom="4px solid #4F82C3";
+     }
+
+}
+
+function bottomborderdisable()
+{
+	 var fixheader=document.getElementsByClassName("fixheader");
+	for(i=0;i<6;i++)
+	{
+		fixheader[i].style.borderBottom="none";
+	}
 }
 
 
